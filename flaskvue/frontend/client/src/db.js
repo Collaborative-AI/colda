9ca37f654320e6fcf939c0654ec0e734dd5ac0b8
xianjian-xie <larry2fox@gmail.com>
2021-09-29 00:00:04 -0500

    newest

--- flaskvue/frontend/client/src/db.js
+++ flaskvue/frontend/client/src/db.js
@@ -1,28 +1,28 @@
 const sqlite3 = window.require('sqlite3').verbose();
 
 
-// let db = new sqlite3.Database('Apollo_Client_db');
+let db = new sqlite3.Database('Apollo_Client_db');
 
-// db.serialize(function() {
-//   db.run("CREATE TABLE IF NOT EXISTS User_Default_Path (id                    int primary key, \
-//                                                    user_id                    int, \
-//                                                    default_train_data_path    text, \
-//                                                    default_train_id_path      text, \
-//                                                    default_test_data_path     text, \
-//                                                    default_test_id_path       text)");
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS User_Default_Path (id                    int primary key, \
+                                                   user_id                    int, \
+                                                   default_train_data_path    text, \
+                                                   default_train_id_path      text, \
+                                                   default_test_data_path     text, \
+                                                   default_test_id_path       text)");
 
-//   db.run("CREATE TABLE IF NOT EXISTS User_Chosen_Path (id     int primary key, \
-//                                            user_id            int, \
-//                                            test_indicator     text, \
-//                                            task_id            text, \
-//                                            test_id            text, \
-//                                            train_data_path    text, \
-//                                            train_id_path      text, \
-//                                            train_target_path  text, \
-//                                            test_data_path     text, \
-//                                            test_id_path       text, \
-//                                            test_target_path   text)")
-// });
-let db=5;
+  db.run("CREATE TABLE IF NOT EXISTS User_Chosen_Path (id     int primary key, \
+                                           user_id            int, \
+                                           test_indicator     text, \
+                                           task_id            text, \
+                                           test_id            text, \
+                                           train_data_path    text, \
+                                           train_id_path      text, \
+                                           train_target_path  text, \
+                                           test_data_path     text, \
+                                           test_id_path       text, \
+                                           test_target_path   text)")
+});
+// let db=5;
 export default db
 // exports.default = db

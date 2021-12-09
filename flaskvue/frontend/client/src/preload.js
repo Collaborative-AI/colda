fb4191fa8ae412983375878f860117044619c24e
1099451900@qq.com <1099451900@qq.com>
2021-12-09 00:59:00 -0600

    ”return_log“

--- flaskvue/frontend/client/src/preload.js
+++ flaskvue/frontend/client/src/preload.js
@@ -4,6 +4,7 @@ window.os = require('os');
 window.node_path = require('path');
 window.dialog = require('electron').remote.dialog;
 window.db = require('better-sqlite3');
+window.log = require('electron-log')
 
 console.log("---", window.fs, window.ex , window.os , window.node_path, window.dialog , window.db)
 

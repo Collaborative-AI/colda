4118597a04f8d0d6dd7e1422030ff47cfb390cd1
Quan Wang <quanwang452@gmail.com>
2022-06-24 18:13:04 -0400

    update FAQ in homepage 7

--- src/components/HomeSeven/Faq.vue
+++ src/components/HomeSeven/Faq.vue
@@ -19,63 +19,75 @@
                         <accordion>
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
-                                    <button class="accordion-title" style = "font-size: 20px;">
-                                        What's The Difference Between IDS And IBS?
+                                    <button class="accordion-title">
+                                        What unique products and services do we provide?
                                         <span></span>
                                         <i class='bx bx-chevron-down'></i>
                                     </button>
                                 </template>
                                 <template v-slot:accordion-content>
-                                    <p>
-                                        Synspot provides Distributed Machine Learning as a Service (DMLasS) via Collaborative Data Analysis (ColDA) to form a machine learning community of shared interest. Synspot enables collaborators to conjointly learn machine learning models without sharing their data, models, and objectives with each other. 
-                                    </p>
+                                    <p>Synspot provides Distributed Machine Learning as a Service (DMLasS) via Collaborative Data Analysis (ColDA) to form a machine learning community of shared interest. Synspot enables collaborators to conjointly learn machine learning models without sharing their data, models, and objectives with each other. </p>
                                 </template>
                             </accordion-item>
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
-                                    <button class="accordion-title" style = "font-size: 20px;">
+                                    <button class="accordion-title">
+                                        What is our motivation?
+                                        <span></span>
+                                        <i class='bx bx-chevron-down'></i>
+                                    </button>
+                                </template>
+                                <template v-slot:accordion-content>
+                                    <ul>
+                                    <li style="color:#666666">It is potentially beneficial to improve machine learning models with external data.</li>
+                                    <li style="color:#666666">It is difficult to leverage external data due to various constraints, i.e. commercial, regulatory, and ethical restrictions.</li>
+                                    <li style="color:#666666">The machine learning community lacks a trustworthy third-party platform to collaboratively analyze data.</li>
+                                    </ul>
+                                </template>
+                            </accordion-item>
+                            <accordion-item>
+                                <template v-slot:accordion-trigger>
+                                    <button class="accordion-title">
                                         What is Collaborative Data Analysis (ColDA)? 
                                         <span></span>
                                         <i class='bx bx-chevron-down'></i>
                                     </button>
                                 </template>
                                 <template v-slot:accordion-content>
-                                    <p>
-                                        Collaborative Data Analysis (ColDA) is a distributed generalization of the well-known Gradient Boosting algorithm which allows participants to collaboratively optimize the aggregate of local loss functions. Each participant autonomously builds its own model by iteratively fitting the pseudo-residuals.
-                                    </p>
+                                    <p>Collaborative Data Analysis (ColDA) is a distributed generalization of the well-known Gradient Boosting algorithm which allows participants to collaboratively optimize the aggregate of local loss functions. Each participant autonomously builds its own model by iteratively fitting the pseudo-residuals.</p>
                                 </template>
                             </accordion-item>
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
-                                    <button class="accordion-title" style = "font-size: 20px;">
+                                    <button class="accordion-title">
                                         What are the advantages of Collaborative Data Analysis (ColDA)?
                                         <span></span>
                                         <i class='bx bx-chevron-down'></i>
                                     </button>
                                 </template>
                                 <template v-slot:accordion-content>
-                                    <p>
-                                        ColDA leverages local resources from participants to utilize the predictive power of external data and reduce computation overhead.
-                                        ColDA generalizes the classical Gradient Boosting algorithm and allows participants to improve machine learning models without necessarily trusting each other.
-                                        ColDA outperforms learning baselines and achieves near-oracle performance on various benchmark datasets. It applies to several real-world use cases such as recommender systems and time-series forecasting.
-                                    </p>
+                                    <ul>
+                                    <li style="color:#666666">ColDA leverages local resources from participants to utilize the predictive power of external data and reduce computation overhead.</li>
+                                    <li style="color:#666666">ColDA generalizes the classical Gradient Boosting algorithm and allows participants to improve machine learning models without necessarily trusting each other.</li>
+                                    <li style="color:#666666">ColDA outperforms learning baselines and achieves near-oracle performance on various benchmark datasets. It applies to several real-world use cases such as recommender systems and time-series forecasting.</li>
+                                    </ul>
                                 </template>
                             </accordion-item>
                             <accordion-item>
                                 <template v-slot:accordion-trigger>
-                                    <button class="accordion-title" style = "font-size: 20px;">
+                                    <button class="accordion-title">
                                         How to get started with Collaborative Data Analysis (ColDA)?
                                         <span></span>
                                         <i class='bx bx-chevron-down'></i>
                                     </button>
                                 </template>
                                 <template v-slot:accordion-content>
-                                    <p>
-                                        Sponsor: A sponsor initiates a learning task and asks for assistance from Assistors.
-                                        Assistor: An assistor provides assistance for a sponsor by locally fitting the pseudo-residuals of the sponsor’s target.
-                                        We provide UI/UX enabled software for quick-start and demonstration. 
-                                        Users who are familiar with data science can directly start with our open-source Python API ColDA. We also provide use cases and documentation of ColDA.
-                                    </p>
+                                    <ul>
+                                    <li style="color:#666666">Sponsor: A sponsor initiates a learning task and asks for assistance from Assistors.</li>
+                                    <li style="color:#666666">Assistor: An assistor provides assistance for a sponsor by locally fitting the pseudo-residuals of the sponsor’s target.</li>
+                                    </ul>
+                                    <p></p>
+                                    <p>We provide UI/UX enabled software for quick-start and demonstration. Users who are familiar with data science can directly start with our open-source Python API ColDA. We also provide use cases and documentation of ColDA.</p>
                                 </template>
                             </accordion-item>
                         </accordion>

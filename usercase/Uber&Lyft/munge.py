0c686e575516b2cad61271f8f97cbea56bc961ea
dinanz <dz1371@nyu.edu>
2022-09-01 13:25:26 -0400

    le clean data

---
+++ usercase/Uber&Lyft/munge.py
@@ -0,0 +1,23 @@
+from sklearn import preprocessing
+import numpy as np
+import pandas as pd
+
+le = preprocessing.LabelEncoder()
+cab = pd.read_csv("input/cab_rides.csv")
+
+# drop useless cols and na records
+cols = ['cab_type', 'id', 'product_id']
+cab.drop(cols, inplace=True, axis=1)
+cab.dropna(axis = 0 , inplace = True)
+
+# transform data to int
+cols = ['destination', 'source', 'name']
+cab[cols] = cab[cols].apply(le.fit_transform).astype(str)
+
+# combine dest and src
+cab['dest_src'] = cab['destination'].str.cat(cab['source'])
+
+cab.drop('destination', inplace=True, axis=1)
+cab.drop('source', inplace=True, axis=1)
+
+cab.to_csv('input/clean_cab_new.csv')
